446b14a23f5e7e7a314e9d05510be493ba6fde64
alxngoga-dev <alxngoga@gmail.com>
2026-06-12 17:39:54 +0200

    adding image uploads and delete

--- src/routes/imageRoute.js
+++ src/routes/imageRoute.js
@@ -20,6 +20,27 @@ router.post(
       res.status(200).json({
         message: "Image uploaded successfully",
         imageUrl: result.secure_url,
+        public_id: result.public_id,
+      });
+    } catch (error) {
+      res.status(500).json({
+        message: error.message,
+      });
+    }
+  }
+);
+
+router.delete(
+  "/delete/:public_id",
+  async (req, res) => {
+    try {
+      const result = await cloudinary.uploader.destroy(
+        req.params.public_id
+      );
+
+      res.status(200).json({
+        message: "Image deleted successfully",
+        result,
       });
     } catch (error) {
       res.status(500).json({
